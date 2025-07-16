f10186c1cac420a669c1744a32f9cf65e2df9f56
Jose Imhoff <jose-imhoff@hotmail.com>
2025-07-16 15:49:41 -0300

    refactor: "Cambio de CV en portfolio"

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -87,7 +87,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap 2 rounded-full outline-none 
         focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="/José_Imhoff_Frontend_dev.pdf"
+          href="/Jose_Imhoff_FrontEnd_Dev_Junior.pdf"
           download
         >
           Download CV{" "}
