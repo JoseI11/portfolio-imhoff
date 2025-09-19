1491376df671cd997d68db8c05627960a8c7ba73
Jose Imhoff <jose-imhoff@hotmail.com>
2025-09-19 20:10:01 -0300

    fix: fix problems of routes in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,42 +8,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'yearly',
       priority: 1,
     },
-    {
-      url: 'https://portfolio-imhoff.vercel.app/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://portfolio-imhoff.vercel.app/projects',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://portfolio-imhoff.vercel.app/skills',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    {
-      url: 'https://portfolio-imhoff.vercel.app/services',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://portfolio-imhoff.vercel.app/experience',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
-      url: 'https://portfolio-imhoff.vercel.app/contact',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.6,
-    },
+    
   ];
 }
-
