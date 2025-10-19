eabe3c813af96253e7830b8d01111011fe2ae4c9
Jose Imhoff <jose-imhoff@hotmail.com>
2025-10-19 20:55:31 -0300

    fix: add some tags to the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <meta charSet="UTF-8" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body
         className={`${geistSans.variable} ${geistMono.variable}  bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
