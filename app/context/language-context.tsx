1c0ed4928e09c38971fda8fef968d7517613742d
Jose Imhoff <jose-imhoff@hotmail.com>
2025-11-06 15:06:45 -0300

    add: create context for the control of languages

---
+++ app/context/language-context.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+
+type Language = "en" | "es";
+
+type LanguageContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type LanguageContextType = {
+  language: Language;
+  toggleLanguage: () => void;
+};
+
+export const LanguageContext = createContext<LanguageContextType | null>(null);
+
+export default function LanguageContextProvider({
+  children,
+}: LanguageContextProviderProps) {
+  const [language, setLanguage] = useState<Language>("en");
+
+  const toggleLanguage = () => {
+    setLanguage((prev) => (prev === "en" ? "es" : "en"));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+
+  if (context === null) {
+    throw new Error("useLanguage must be used within a LanguageContextProvider");
+  }
+
+  return context;
+}
